5a14eb191a029e5d7b2f70bab229770b4a72356a
ThomasYemane <117491955+ThomasYemane@users.noreply.github.com>
2024-08-29 20:16:13 -0700

    Initial commit

---
+++ problems/02-repeat-name.js
@@ -0,0 +1,23 @@
+/*
+Assign the variables repeatDaniel, repeatMark, and repeatBeyonce to values
+by calling the repeatName(name) function so that the console.logs will
+print out the expected values below.
+*/
+
+function repeatName(name) {
+  return name + " " + name;
+}
+
+// Your code here 
+
+
+let repeatDaniel = repeatName("Daniel");
+let repeatMark = repeatName("Mark");
+let repeatBeyonce = repeatName("!!!Beynoce");
+
+console.log(repeatDaniel);  // => "Daniel Daniel"
+console.log(repeatMark);    // => "Mark Mark"
+console.log(repeatBeyonce); // => "!!!Beyonce Beyonce!!!"
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+module.exports = repeatName;
